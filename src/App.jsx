87b95912ee0ce58a05804051a6b19a5796847cbd
gogi999 <g.petrovic9@gmail.com>
2023-09-11 14:11:40 +0200

    Started to implement styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import styled from 'styled-components';
+
+import { GlobalStyles } from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Input from './ui/Input';
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>The Wild Oasis</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check in")}>
+          Check in
+        </Button>
+        <Button onClick={() => alert("Check out")}>
+          Check out
+        </Button>
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
